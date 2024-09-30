73930a12c77288c02ef273e9cfae00ce72f70eee
kyouju1231 <harada12.31.a@gmail.com>
2024-09-30 15:59:05 +0900

    麻痺を演算要素に追加

--- coin_sim.py
+++ coin_sim.py
@@ -5,10 +5,10 @@ import math
 def binomial_probability(n, k, p):
     """ 二項分布の確率を計算する関数
         n:試行回数  k:成功回数  p:試行1回の成功確率 """
-    # リンバスにおいては
-    # n:コイン枚数  k:表コイン枚数  p:表面が出る確率
+        # リンバスにおいては
+        # n:コイン枚数  k:表コイン枚数  p:表面が出る確率
     combination = math.factorial(n) / ( math.factorial(k) * math.factorial(n-k) )
-    return combination * (p**k) * ( (1-p) ** (n-k))
+    return combination * (p**k) * ( (1-p) ** (n-k) )
 
 
 class skill:
@@ -19,12 +19,15 @@ class skill:
         coin_power  :int,
         coin_count  :int,
         mental      :int,
+        paralyze    :int,
     ):
 
-        self.bp = base_power
-        self.cp = coin_power
-        self.cc = coin_count
+        self.bp  = base_power
+        self.cp  = coin_power
+        self.cc  = coin_count
         self.men = mental
+        self.prz = paralyze
+
         self.results =[]
         self.cal_final_power_and_probability()
 
@@ -32,54 +35,90 @@ class skill:
         # コインが表になる確率
         coin_heads_prob = (self.men + 50) / 100
 
-        # 表コインの枚数の組み合わせを生成 表0枚～n枚まで
-        heads_counts = range(self.cc + 1)
-
-        # 各組み合わせの最終威力と確率を計算
         self.results = []
-        for heads_count in heads_counts:
-            # 表0枚の時の最終威力とその確率から表n枚までを計算
+        # 最終威力とそれが出る確率の組み合わせが全て格納されるリスト
 
-            final_power = self.bp + (heads_count * self.cp)
-            # 最終威力計算  基礎威力+( 表の枚数 x コイン威力 )
+        # 麻痺を考慮した補正コイン枚数を算出
+        ccc = self.cc - self.prz
+        # ccc: correction coin counts  補正コイン枚数
+            # 麻痺は裏面固定と同義なので
+            # コイン枚数を麻痺の数値分減らす事で組み合わせを計算できる
 
-            probability = binomial_probability(self.cc, heads_count, coin_heads_prob)
-            # 最終威力が出る確率
+        if ccc > 0:
+        # コイン枚数>麻痺の場合
 
-            self.results.append( (final_power, probability) )
-            # 最終威力とその確率をタプル型でリザルトリストに追加
-            # 最終威力とそれが出る確率の組み合わせが全て格納される
+            heads_counts = range(ccc + 1)
+            # 表コインの枚数の組み合わせを生成 表0枚～n枚まで
+
+            # 各組み合わせの最終威力と確率を計算
+            for heads_count in heads_counts:
+
+                final_power = self.bp + (heads_count * self.cp)
+                # 最終威力計算  基礎威力+( 表の枚数 x コイン威力 )
+                if final_power < 0:
+                    final_power = 0     # 威力が0以下なら0として判定
 
 
-def cal_match(ally_skill:skill, enemy_skill:skill):
+                probability = binomial_probability(ccc, heads_count, coin_heads_prob)
+                # 最終威力が出る確率
+
+                self.results.append( (final_power, probability) )
+                # 最終威力とその確率をタプル型でリザルトリストに追加
+
+        else:
+        # 麻痺>=コイン枚数の場合
+            self.results.append( (self.bp, 1) )
+            # 全てのコインが麻痺によって威力0になる
+            # 100%の確率で基礎威力の値が最終威力になる
+
+
+def cal_match(
+        a_bp, a_cp, a_cc, a_men, a_prz,
+        e_bp, e_cp, e_cc, e_men, e_prz,
+    ):
     """ スキルマッチ1回分の勝率計算 """
-    # どちらのコインが破壊されるかの確率
-    win_prob = 0
+
+    ally_skill = skill(a_bp, a_cp, a_cc, a_men, a_prz)
+    enemy_skill= skill(e_bp, e_cp, e_cc, e_men, e_prz)
+
+    # 勝率敗率引分率
+    win_prob  = 0
     lose_prob = 0
+    draw_prob = 0
 
     for (ally_fp, ally_prob) in ally_skill.results:
         for (enemy_fp, enemy_prob) in enemy_skill.results:
 
             # 確率
-            probability = ally_prob*enemy_prob
+            probability = ally_prob * enemy_prob
 
             # 勝敗の比較
-            if ally_fp > enemy_fp:      # 勝率
+            if ally_fp > enemy_fp:          # 勝率
                 win_prob += probability
-            elif ally_fp < enemy_fp:   # 敗率
+            elif ally_fp < enemy_fp:        # 敗率
                 lose_prob += probability
             else:
                 pass
 
     # マッチ勝率
-    final_probability = win_prob / ( win_prob+lose_prob )
+    if a_prz == 0 and e_prz == 0:
+        # 麻痺が無ければ引き分けの可能性を消す
+        # 永遠に引き分ける可能性を残すと
+        # cal_win_probでの再帰計算が終わらないので引き分けの可能性をここで消す
+        win_prob  = win_prob / ( win_prob + lose_prob )
+        lose_prob = 1 - win_prob
+    else:
+        # 麻痺があれば次のマッチで麻痺が減る事で
+        # 勝率が変わるので引き分けも含めて勝率を出す
+        draw_prob = 1 - ( win_prob + lose_prob )
 
-    return final_probability
+
+    return win_prob, lose_prob, draw_prob
 
 
 def cal_win_prob(
-        a_bp, a_cp, a_cc, a_m,
-        e_bp, e_cp, e_cc, e_m
+        a_bp, a_cp, a_cc, a_men, a_prz,
+        e_bp, e_cp, e_cc, e_men, e_prz,
     ):
     """ 最終勝率計算 """
     if a_cc == 0:
@@ -92,46 +131,80 @@ def cal_win_prob(
 
     else:
     # それ以外の場合、再帰的に勝率を計算
-        ally_skill = skill(a_bp, a_cp, a_cc, a_m)
-        enemy_skill = skill(e_bp, e_cp, e_cc, e_m)
+        # 今回のマッチ確率
+        prob_A, prob_E, prob_d = cal_match(
+            a_bp, a_cp, a_cc, a_men, a_prz,
+            e_bp, e_cp, e_cc, e_men, e_prz
+        )
 
-        # Allyが勝つ確率
-        prob_A = cal_match(ally_skill,enemy_skill)
-        # Enemyが勝つ確率
-        prob_E = 1 - prob_A
+        # 次のマッチの麻痺値の処理
+        if a_prz > 0:
+        # コインを振った分だけ麻痺値を減少
+            a_prz = a_prz - a_cc
+            if a_prz < 0:
+            # 麻痺0未満になったら麻痺を0に
+                a_prz = 0
 
-        # Aが勝つ場合
+        if e_prz > 0:
+        # コインを振った分だけ麻痺値を減少
+            e_prz = e_prz - e_cc
+            if e_prz < 0:
+            # 麻痺0未満になったら麻痺を0に
+                e_prz = 0
+
+
+        # マッチでAllyが勝つ場合
         prob_A_wins = (
             prob_A * cal_win_prob(
-                        a_bp, a_cp, a_cc, a_m,
-                        e_bp, e_cp, e_cc-1, e_m
+                        a_bp, a_cp, a_cc  , a_men, a_prz,
+                        e_bp, e_cp, e_cc-1, e_men, e_prz,
             )
+            # allyが勝つので次のマッチはenemyのコインが1枚減る
         )
 
-        # Eが勝つ場合
-        prob_B_wins = (
+        # マッチでEnemyが勝つ場合
+        prob_E_wins = (
             prob_E * cal_win_prob(
-                        a_bp, a_cp, a_cc-1, a_m,
-                        e_bp, e_cp, e_cc, e_m
+                        a_bp, a_cp, a_cc-1, a_men, a_prz,
+                        e_bp, e_cp, e_cc  , e_men, e_prz,
             )
+            # enemyが勝つので次のマッチはallyのコインが1枚減る
         )
+
+        # マッチで引き分ける場合
+        if prob_d != 0:
+            # 引き分ける時だけ計算する
+            # cal_matchは麻痺が1以上の時だけ引き分けの確率を出す
+            prob_draw = (
+                prob_d * cal_win_prob(
+                            a_bp, a_cp, a_cc, a_men, a_prz,
+                            e_bp, e_cp, e_cc, e_men, e_prz,
+                )
+                # 引き分けるのでコインは減らない
+            )
+        else:
+            prob_draw = prob_d
+
 
         # 最終的な勝率を計算して返す
-        return prob_A_wins + prob_B_wins
+        return prob_A_wins + prob_E_wins + prob_draw
 
 
 
 def calcutate(ally_data, enemy_data):
+    """ bp, cp, cc, men, prz """
     try:
         ally_base_power = int(ally_data[0])
         ally_coin_power = int(ally_data[1])
         ally_coin_count = int(ally_data[2])
         ally_mental     = int(ally_data[3])
+        ally_paralyze   = int(ally_data[4])
 
         enemy_base_power = int(enemy_data[0])
         enemy_coin_power = int(enemy_data[1])
         enemy_coin_count = int(enemy_data[2])
         enemy_mental     = int(enemy_data[3])
+        enemy_paralyze   = int(enemy_data[4])
 
     except ValueError:
         return -2,0
@@ -149,27 +222,36 @@ def calcutate(ally_data, enemy_data):
 
 
     # 初回マッチ勝率
-    ally_skill = skill(
-        ally_base_power,
-        ally_coin_power,
-        ally_coin_count,
-        ally_mental
+    win_prob, lose_prob, draw_prob = cal_match(
+        ally_base_power, ally_coin_power, ally_coin_count, ally_mental, ally_paralyze,
+        enemy_base_power, enemy_coin_power, enemy_coin_count, enemy_mental, enemy_paralyze,
     )
-    enemy_skill = skill(
-        enemy_base_power,
-        enemy_coin_power,
-        enemy_coin_count,
-        enemy_mental
-    )
-    result1 = cal_match(ally_skill,enemy_skill)
+
+    if draw_prob == 0:
+        first_probability = win_prob
+    else:
+        # 引き分けの確率があるならそれを消して勝率を収束させる
+        first_probability  = win_prob / ( win_prob + lose_prob )
+
 
     # 最終勝率
-    result2 = cal_win_prob(
-        ally_base_power, ally_coin_power, ally_coin_count, ally_mental,
-        enemy_base_power, enemy_coin_power, enemy_coin_count, enemy_mental
+    final_probability = cal_win_prob(
+        ally_base_power, ally_coin_power, ally_coin_count, ally_mental, ally_paralyze,
+        enemy_base_power, enemy_coin_power, enemy_coin_count, enemy_mental, enemy_paralyze,
     )
 
 
+    return first_probability, final_probability
 
 
-    return result1,result2
+# 動作テスト
+if __name__ == '__main__':
+    #       (基礎威力, コイン威力, コイン数, 精神力, 麻痺)
+    ally  = ( 5, 1, 3, 0, 0 )
+    enemy = ( 5, 1, 4, 0, 3 )
+
+    result = calcutate(ally,enemy)
+    print(
+        f"初回勝率:{result[0]}\n"
+        f"最終勝率:{result[1]}"
+    )
